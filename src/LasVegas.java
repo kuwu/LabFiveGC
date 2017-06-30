7c7c239390f50f89f9c87d22b64613ffd110c420
Kurt Wunderlich <kuwu@mac.com>
2017-06-29 22:45:11 -0400

    first commit

---
+++ src/LasVegas.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/**
+ * Created by kuwu on 2017/06/29.
+ */
+public class LasVegas {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // created a class called Parse so it can be reused
+        // instantiate Class Parse as parse object
+        Parse parse = new Parse();
+
+        // called method for getting die sides and rolling dice
+        getUserInput(scan);
+
+        // validating for y / n using Parse Class
+        while (parse.parse()){
+            // if true, get input and roll dice again
+            getUserInput(scan);
+        }
+
+
+
+
+    }
+    // using Math.random to create the dice rolls
+    public static int getTheRoll(int dieSides) {
+        int dieRoll = (int) (Math.random() * ((dieSides - 1) + 1) + 1);
+        return dieRoll;
+    }
+
+    // using Method to group Scanner, input, dice rolls and printout
+    public static void getUserInput(Scanner scan){
+        System.out.println("How many sides should the dice have?");
+        int sides = scan.nextInt();
+
+        int die1 = getTheRoll(sides);
+        int die2 = getTheRoll(sides);
+        System.out.println("\n Rolling the dice ...........\n");
+        System.out.println("Die 1:  " + die1);
+        System.out.println("Die 2:  " + die2);
+
+    }
+
+}
+
+
+
+
---
+++ src/Parse.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * Created by kuwu on 2017/06/29.
+ */
+public class Parse {
+
+        // validation of y / n for continue, stop or error input
+        public static boolean parse() {
+            char userChar;
+            boolean value = true;
+            System.out.println("\n To continue, please enter:   (y/n)");
+            Scanner scan = new Scanner(System.in);
+
+            boolean returnValue = false;
+
+            do {
+                userChar = scan.next().charAt(0);
+                // check for Y or y to continue
+                if ((Character.toLowerCase(userChar))== 'y') {
+                    value = false;
+                    returnValue = true;
+                    // check for N or n to stop
+                } else if ((Character.toLowerCase(userChar))== 'n') {
+                    value = false;
+                    returnValue = false;
+                    // error output for any input other than Yy/Nn
+                } else {
+                    System.out.println(userChar + " is not y or n please re-enter");
+                }
+
+            } while (value);
+
+            return returnValue;
+        }
+
+}
